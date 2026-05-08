d54d3650399b02fe21794d9b7252bdc429be6115
Arthur Felipe de Freitas Morais <arthurfmorais05@gmail.com>
2026-05-07 22:13:04 -0300

    Update and rename Gama.hpp to Gama_c.hpp

--- include/Gama_c.hpp
+++ include/Gama_c.hpp
@@ -3,16 +3,27 @@
 #include "DistribuicaoContinua.hpp"
  
 class Gama : public DistribuicaoContinua {
-private:
-    double forma;
-    double taxa;
- 
 public:
     Gama(double forma, double taxa);
+ 
     double densidade(double x) const;
+
     double acumulada(double x) const;
-    double media() const;
+
+    double esperanca() const;
+
     double variancia() const;
+
+    std::string nome() const;
+
     void exibir() const;
+ 
+    double getForma() const;
+
+    double getTaxa() const;
+ 
+private:
+    double forma_;
+    double taxa_;
 };
 #endif
