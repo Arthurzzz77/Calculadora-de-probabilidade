4ea6b04073ea508084433573a934ceb52f3975ce
Arthur Felipe de Freitas Morais <arthurfmorais05@gmail.com>
2026-05-07 22:02:07 -0300

    Update Distribuicaocontinua.hpp

--- include/Distribuicaocontinua.hpp
+++ include/Distribuicaocontinua.hpp
@@ -1,10 +1,14 @@
 #ifndef DISTRIBUICAO_CONTINUA_HPP
 #define DISTRIBUICAO_CONTINUA_HPP
 #include "Distribuicao.hpp"
- 
+
 class DistribuicaoContinua : public Distribuicao {
 public:
-    virtual ~DistribuicaoContinua() {}
+    virtual ~DistribuicaoContinua() = default;
+
+    virtual double densidade(double x) const = 0;
+
+    virtual double acumulada(double x) const = 0;
 
     virtual double probabilidadeIntervalo(double a, double b) const;
 };
