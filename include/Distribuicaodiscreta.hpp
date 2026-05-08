51b0c262b8899706384271acec6a1c86902ae702
Arthur Felipe de Freitas Morais <arthurfmorais05@gmail.com>
2026-05-07 22:01:03 -0300

    Update Distribuicaodiscreta.hpp

--- include/Distribuicaodiscreta.hpp
+++ include/Distribuicaodiscreta.hpp
@@ -1,15 +1,15 @@
 #ifndef DISTRIBUICAO_DISCRETA_HPP
 #define DISTRIBUICAO_DISCRETA_HPP
 #include "Distribuicao.hpp"
-
+ 
 class DistribuicaoDiscreta : public Distribuicao {
 public:
-    virtual ~DistribuicaoDiscreta() {}
+    virtual ~DistribuicaoDiscreta() = default;
 
     virtual double probabilidade(int k) const = 0;
- 
-    double densidade(double x) const{
-      return probabilidade(static_cast<int>(x));
-      }
+
+    virtual double acumulada(int k) const;
+
+    virtual double probabilidadeIntervalo(int a, int b) const;
 };
 #endif
