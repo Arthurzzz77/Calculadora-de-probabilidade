b0c873235e12a533c4ba7ab7bb930e69b9488d7f
Arthur Felipe de Freitas Morais <arthurfmorais05@gmail.com>
2026-05-07 22:03:52 -0300

    Update Bernoulli.hpp

--- include/Bernoulli.hpp
+++ include/Bernoulli.hpp
@@ -1,18 +1,26 @@
 #ifndef BERNOULLI_HPP
 #define BERNOULLI_HPP
- 
 #include "DistribuicaoDiscreta.hpp"
-
-class Bernoulli : public DistribuicaoDiscreta {
-private:
-    double chanceSucesso; 
-public:
-Bernoulli(double chanceSucesso);
-  double probabilidade(int k) const override;
-  double acumulada(double x) const override;
-  double media() const override;
-  double variancia() const override;
-  void exibir() const override;
-};
  
+class Bernoulli : public DistribuicaoDiscreta {
+public:
+    explicit Bernoulli(double chanceSucesso);
+
+    double probabilidade(int k) const;
+
+    double acumulada(int k) const;
+
+    double esperanca() const;
+
+    double variancia() const;
+
+    std::string nome() const;
+
+    void exibir() const;
+
+    double getChanceSucesso() const;
+
+private:
+    double chanceSucesso_;
+};
 #endif
