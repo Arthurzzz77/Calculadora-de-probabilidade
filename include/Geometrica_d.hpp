74f5be8e51c9c07622212c0f17dc5787a89ab177
JoaoSaturnino05 <joaopedrokakehasi@gmail.com>
2026-06-09 20:16:26 -0300

    Update Geometrica_d.hpp

--- include/Geometrica_d.hpp
+++ include/Geometrica_d.hpp
@@ -2,17 +2,30 @@
 #define GEOMETRICA_HPP
 #include "DistribuicaoDiscreta.hpp"
 
+/**
+ * @brief Distribuicao Geometrica (numero de tentativas ate o 1o sucesso).
+ *
+ * Suporte: k = 1, 2, 3, ...   P(X = k) = (1 - p)^(k-1) * p
+ */
 class Geometrica : public DistribuicaoDiscreta {
 public:
+    /**
+     * @brief Constroi uma distribuicao Geometrica.
+     * @param p Probabilidade de sucesso em cada tentativa, no intervalo (0, 1].
+     * @throws std::invalid_argument se p nao estiver em (0, 1].
+     */
     explicit Geometrica(double p);
-    double pmf(int k);
-    double cdf(int k);
-    double esperanca();
-    double variancia();
-    std::string nome();
-    double getP();
+
+    double probabilidade(int k) const override;
+    double acumulada(int k) const override;
+    double esperanca() const override;
+    double variancia() const override;
+    std::string nome() const override;
+    void exibir() const override;
+
+    double getP() const;
+
 private:
     double p_;
 };
-
 #endif
