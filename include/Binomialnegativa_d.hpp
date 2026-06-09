51e5663f20df7c9f27108b2dfd0a92223d75cb27
JoaoSaturnino05 <joaopedrokakehasi@gmail.com>
2026-06-09 20:24:56 -0300

    Update Binomialnegativa_d.hpp

--- include/Binomialnegativa_d.hpp
+++ include/Binomialnegativa_d.hpp
@@ -1,20 +1,34 @@
-// include/BinomialNegativa.hpp
 #ifndef BINOMIAL_NEGATIVA_HPP
 #define BINOMIAL_NEGATIVA_HPP
 #include "DistribuicaoDiscreta.hpp"
 
+/**
+ * @brief Distribuicao Binomial Negativa (numero de tentativas ate o r-esimo sucesso).
+ *
+ * Suporte: k = r, r+1, ...   P(X = k) = C(k-1, r-1) * p^r * (1-p)^(k-r)
+ */
 class BinomialNegativa : public DistribuicaoDiscreta {
 public:
+    /**
+     * @brief Constroi uma distribuicao Binomial Negativa.
+     * @param r Numero de sucessos desejados (inteiro >= 1).
+     * @param p Probabilidade de sucesso em cada tentativa, no intervalo (0, 1].
+     * @throws std::invalid_argument se r < 1 ou p nao estiver em (0, 1].
+     */
     BinomialNegativa(int r, double p);
-    double pmf(int k);
-    double esperanca();
-    double variancia();
-    std::string nome();
-    int getR();
-    double getP();
+
+    double probabilidade(int k) const override;
+    double acumulada(int k) const override;
+    double esperanca() const override;
+    double variancia() const override;
+    std::string nome() const override;
+    void exibir() const override;
+
+    int getR() const;
+    double getP() const;
+
 private:
     int r_;
     double p_;
 };
-
 #endif
